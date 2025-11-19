18abb1a318679399dbc3d5dcaa27dbcc062a556d
fiiiore <69092190+fiiiore@users.noreply.github.com>
2025-11-19 16:40:37 -0300

    push
    push

---
+++ analisis.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import networkx as nx
+
+
+creditos = pd.read_csv("datos/tmdb_5000_credits.csv")
+peliculas = pd.read_csv("datos/tmdb_5000_movies.csv")
+print("peliculas:")
+print(peliculas.head())
+print("creditos:")
+print(creditos.head())
